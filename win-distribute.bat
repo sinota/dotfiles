3800fbd487b4bd8ceb6e4f404cecfa298c0d8b8e
Shinsuke Ota <ot.snsk@gmail.com>
2015-03-23 17:07:33 +0900

    append 3 dotfiles using on windows.

--- win-distribute.bat
+++ win-distribute.bat
@@ -12,7 +12,9 @@ rem runas /user:administrator "mklink %DST%_vimrc %HERE%.vimrc"
 
 rem /Y: force overwrite
 copy /-Y %HERE%.vimrc %DST%.vimrc
-
+copy /-Y %HERE%.profile_win %DST%.profile
+copy /-Y %HERE%.bashrc_win %DST%.bashrc
+copy /-Y %HERE%.bash_profile_win %DST%.bash_profile
 
 pause
 exit
