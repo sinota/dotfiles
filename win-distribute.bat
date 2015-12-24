234e4b3d4f678c9579239917f6710d3f189b1ca7
Shinsuke Ota <ot.snsk@gmail.com>
2015-12-24 15:41:31 +0900

    zshrcとか追加 README 中身なし

--- win-distribute.bat
+++ win-distribute.bat
@@ -10,11 +10,13 @@ set DST=%HOME%
 rem Need Administrator privilege + password 
 rem runas /user:administrator "mklink %DST%_vimrc %HERE%.vimrc"
 
-rem /Y: force overwrite
-copy /-Y %HERE%.vimrc %DST%.vimrc
-copy /-Y %HERE%.profile_win %DST%.profile
-copy /-Y %HERE%.bashrc_win %DST%.bashrc
-copy /-Y %HERE%.bash_profile_win %DST%.bash_profile
+rem /Y: force overwrite  /-Y: require confirm
+xcopy /D /Y %HERE%.vimrc %DST%.vimrc
+xcopy /D /Y %HERE%.profile_win %DST%.profile
+xcopy /D /Y %HERE%.bashrc_win %DST%.bashrc
+xcopy /D /Y %HERE%.bash_profile_win %DST%.bash_profile
+xcopy /D /Y %HERE%.zshrc %DST%.zshrc
+
 
 pause
 exit
