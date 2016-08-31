e2c9f165b531faec66e4fc9334d0cd95e1f7cba9
Shinsuke Ota <ot.snsk@gmail.com>
2016-08-31 19:34:49 +0900

    Current.

--- win-distribute.bat
+++ win-distribute.bat
@@ -17,6 +17,7 @@ xcopy /D /Y %HERE%.bashrc_win %DST%.bashrc
 xcopy /D /Y %HERE%.bash_profile_win %DST%.bash_profile
 xcopy /D /Y %HERE%.zshrc_win %DST%.zshrc
 xcopy /D /Y %HERE%.tmux.win.conf %DST%.tmux.conf
+xcopy /D /Y %HERE%.minttyrc %DST%.minttyrc
 
 
 pause
