f495e5522750e326b374e903257039968d1ab291
Shinsuke Ota <ot.snsk@gmail.com>
2018-11-15 11:22:22 +0900

    New .bash_aliases for the new chroot environment.

---
+++ bash_aliases-xenial-cli.sh
@@ -0,0 +1,11 @@
+# alias ls="ls --color=auto -F"
+
+alias remountsd="sudo mount -o remount,exec /media/removable/MYCARD"
+alias enterpy35="source /media/removable/MYCARD/pyenv/py35/bin/activate"
+alias w3mbookmark="w3m -B"
+alias tmux="tmux -u"
+alias g++11="g++ -std=c++11"
+
+alias aptse="apt-cache search"
+alias aptsh="apt-cache show"
+
